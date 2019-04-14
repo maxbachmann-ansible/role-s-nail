d0848cc874e9d0ff60623cbd7e560824c3d825fd
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-04-14 16:54:35 +0200

    Update shell-login.sh

--- templates/shell-login.sh
+++ templates/shell-login.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Login auf {{ server_name }} am $(date +%Y-%m-%d) um $(date +%H:%M)"
+echo "Login auf $(hostname) am $(date +%Y-%m-%d) um $(date +%H:%M)"
 echo "Benutzer: $USER"
 echo
 pinky
